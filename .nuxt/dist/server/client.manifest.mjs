a090f7087b41ca7b740e60dab6c417682c01bc36
dapobelieve <dapomichaels@gmail.com>
2026-04-24 17:37:00 +0100

    feat: add user-path linking, .gitignore, remove node_modules from tracking

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///Users/believe/Work/lxpath/admin/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/believe/Work/lxpath/admin/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
